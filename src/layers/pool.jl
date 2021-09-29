e23036a5d6d59baf5f8a0828dc2e8e7cf18711f8
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-09-29 17:49:29 +0200

    use readout_nodes in pooling

--- src/layers/pool.jl
+++ src/layers/pool.jl
@@ -33,14 +33,8 @@ struct GlobalPool{F} <: GNNLayer
     aggr::F
 end
 
-function (l::GlobalPool)(g::GNNGraph, X::AbstractArray)
-    if isnothing(g.graph_indicator)
-        # assume only one graph
-        indexes = fill!(similar(X, Int, g.num_nodes), 1)     
-    else 
-        indexes = g.graph_indicator
-    end
-    return NNlib.scatter(l.aggr, X, indexes)
+function (l::GlobalPool)(g::GNNGraph, x::AbstractArray)
+    return readout_nodes(g, x, l.aggr)
 end
 
 """
