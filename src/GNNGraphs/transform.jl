d17be36b5f8b0087599e05c98ed4650de858e82c
CarloLucibello <carlo.lucibello@gmail.com>
2022-01-23 15:25:09 +0100

    docs

--- src/GNNGraphs/transform.jl
+++ src/GNNGraphs/transform.jl
@@ -156,10 +156,61 @@ end
 #     g.num_edges += length(snew)
 #     return true
 # end
-"""
-    to_bidirected(g; aggr=mean)
 
-See also [`is_bidirected`](@ref) and [`remove_multi_edges`](@ref). 
+"""
+    to_bidirected(g)
+
+Adds a reverse edge for each edge in the graph, then calls 
+[`remove_multi_edges`](@ref) with `mean` aggregation to simplify the graph. 
+
+See also [`is_bidirected`](@ref). 
+
+# Examples
+
+```juliarepl
+julia> s, t = [1, 2, 3, 3, 4], [2, 3, 4, 4, 4];
+
+julia> w = [1.0, 2.0, 3.0, 4.0, 5.0];
+
+julia> e = [10.0, 20.0, 30.0, 40.0, 50.0];
+
+julia> g = GNNGraph(s, t, w, edata = e)
+GNNGraph:
+    num_nodes = 4
+    num_edges = 5
+    edata:
+        e => (5,)
+
+julia> g2 = to_bidirected(g)
+GNNGraph:
+    num_nodes = 4
+    num_edges = 7
+    edata:
+        e => (7,)
+
+julia> edge_index(g2)
+([1, 2, 2, 3, 3, 4, 4], [2, 1, 3, 2, 4, 3, 4])
+
+julia> get_edge_weight(g2)
+7-element Vector{Float64}:
+ 1.0
+ 1.0
+ 2.0
+ 2.0
+ 3.5
+ 3.5
+ 5.0
+
+julia> g2.edata.e
+7-element Vector{Float64}:
+ 10.0
+ 10.0
+ 20.0
+ 20.0
+ 35.0
+ 35.0
+ 50.0
+```
 """
 function to_bidirected(g::GNNGraph{<:COO_T})
     s, t = edge_index(g)
--- test/GNNGraphs/transform.jl
+++ test/GNNGraphs/transform.jl
@@ -212,7 +212,10 @@
 
     @testset "to_bidirected" begin
         if GRAPH_T == :coo
-            g = GNNGraph([1,2,3,3,4], [2,3,4,4,4], [1.0,2.0,3,4,5])
+            s, t = [1, 2, 3, 3, 4], [2, 3, 4, 4, 4] 
+            w = [1.0, 2.0, 3.0, 4.0, 5.0]
+            e = [10.0, 20.0, 30.0, 40.0, 50.0]
+            g = GNNGraph(s, t, w, edata = e)
 
             g2 = to_bidirected(g)
             @test g2.num_nodes == g.num_nodes
@@ -226,6 +229,7 @@
             @test s2 == [1, 2, 2, 3, 3, 4, 4]
             @test t2 == [2, 1, 3, 2, 4, 3, 4]
             @test w2 == [1, 1, 2, 2, 3.5, 3.5, 5]
+            @test g2.edata.e == [10.0, 10.0, 20.0, 20.0, 35.0, 35.0, 50.0]
         end
     end
 end
