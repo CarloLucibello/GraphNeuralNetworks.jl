5b32a12b621e9097c02273c227e9427b467eadff
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-02-25 07:40:32 +0100

    add benchmarks

---
+++ benchmark/runbenchmarks.jl
@@ -0,0 +1,13 @@
+using PkgBenchmark
+
+benchmarkpkg(
+    dirname(@__DIR__),
+    BenchmarkConfig(
+        id = "v0.3.13",
+        env = Dict(
+            "JULIA_NUM_THREADS" => "1",
+            "OMP_NUM_THREADS" => "1",
+        ),
+    ),
+    resultfile = joinpath(@__DIR__, "result.json"),
+)
