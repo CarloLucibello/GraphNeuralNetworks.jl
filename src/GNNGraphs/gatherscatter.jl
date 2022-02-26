9157aa9337cac96b145254a508bbc0a8cf17b67b
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-02-26 07:40:12 +0100

    fix

--- src/GNNGraphs/gatherscatter.jl
+++ src/GNNGraphs/gatherscatter.jl
@@ -3,15 +3,20 @@ _gather(x::Tuple, i) = map(x -> _gather(x, i), x)
 _gather(x::AbstractArray, i) = NNlib.gather(x, i)
 _gather(x::Nothing, i) = nothing
 
-_scatter(aggr, m::Nothing, t; dstsize=nothing) = nothing
-_scatter(aggr, m::NamedTuple, t; dstsize=nothing) = map(m -> _scatter(aggr, m, t; dstsize), m)
-_scatter(aggr, m::Tuple, t; dstsize=nothing) = map(m -> _scatter(aggr, m, t; dstsize), m)
-function _scatter(aggr, m::AbstractMatrix, t; dstsize=nothing)
-    if dstsize !== nothing
-        dstsize = (size(m,1), dstsize)
-    end
-    NNlib.scatter(aggr, m, t; dstsize)
+_scatter(aggr, src::Nothing, idx, n) = nothing
+_scatter(aggr, src::NamedTuple, idx, n) = map(s -> _scatter(aggr, s, idx, n), src)
+_scatter(aggr, src::Tuple, idx, n) = map(s -> _scatter(aggr, s, idx, n), src)
+
+function _scatter(aggr, 
+                 src::AbstractArray, 
+                 idx::AbstractVector{<:Integer}, 
+                 n::Integer)
+
+    dstsize =  (size(src)[1:end-1]..., n)
+    NNlib.scatter(aggr, src, idx; dstsize)
 end
+
+
 ## TO MOVE TO NNlib ######################################################
 
 
