5e589b57a9321ce898de0784c4e1863d05a83e4c
Mohit Anand <itsmohitanand@gmail.com>
2022-03-22 16:20:38 +0100

    Update src/layers/conv.jl
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- src/layers/conv.jl
+++ src/layers/conv.jl
@@ -1134,7 +1134,7 @@ function GMMConv(ch::Pair{NTuple{2,Int},Int},
     (nin, ein), out = ch
     mu = init(ein, K)
     sigma_inv = init(ein, K)
-    b = bias ? Flux.create_bias(ones(out), true) : false
+    b = bias ? Flux.create_bias(mu, true, out) : false
     dense_x = Dense(nin, out*K, bias=false)
     GMMConv(mu, sigma_inv, b, σ, ch, K, dense_x, residual)
 end
