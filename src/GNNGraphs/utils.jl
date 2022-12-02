ba412e9c0a5ba649ca7978c59e13f811ca72f447
aurorarossi <aurora98rossi@gmail.com>
2022-12-02 18:21:17 +0100

    Separate commit for formatting

--- src/GNNGraphs/utils.jl
+++ src/GNNGraphs/utils.jl
@@ -11,7 +11,7 @@ check_num_nodes(::GNNGraph, ::Nothing) = true
 
 function check_num_edges(g::GNNGraph, e::AbstractArray)
     @assert g.num_edges == size(e, ndims(e)) "Got $(size(e, ndims(e))) as last dimension size instead of num_edges=$(g.num_edges)"
-    return true    
+    return true
 end
 function check_num_edges(g::GNNGraph, x::Union{Tuple,NamedTuple})
     map(x -> check_num_edges(g, x), x)
@@ -34,9 +34,9 @@ function sort_edge_index(u::AnyCuArray, v::AnyCuArray)
     sort_edge_index(u |> Flux.cpu, v |> Flux.cpu) |> Flux.gpu
 end
 
-cat_features(x1::Nothing, x2::Nothing) = nothing 
+cat_features(x1::Nothing, x2::Nothing) = nothing
 cat_features(x1::AbstractArray, x2::AbstractArray) = cat(x1, x2, dims=ndims(x1))
-cat_features(x1::Union{Number, AbstractVector}, x2::Union{Number, AbstractVector}) = 
+cat_features(x1::Union{Number, AbstractVector}, x2::Union{Number, AbstractVector}) =
     cat(x1, x2, dims=1)
 
 # workaround for issue #98 #104
@@ -45,12 +45,12 @@ cat_features(xs::AbstractVector{NamedTuple{(), Tuple{}}}) = (;)
 
 function cat_features(x1::NamedTuple, x2::NamedTuple)
     sort(collect(keys(x1))) == sort(collect(keys(x2))) || @error "cannot concatenate feature data with different keys"
-    
+
     NamedTuple(k => cat_features(getfield(x1,k), getfield(x2,k)) for k in keys(x1))
 end
 
 function cat_features(xs::AbstractVector{<:AbstractArray{T,N}}) where {T<:Number, N}
-   cat(xs...; dims=N) 
+   cat(xs...; dims=N)
 end
 
 cat_features(xs::AbstractVector{Nothing}) = nothing
@@ -59,9 +59,9 @@ cat_features(xs::AbstractVector{<:Number}) = xs
 function cat_features(xs::AbstractVector{<:NamedTuple})
     symbols = [sort(collect(keys(x))) for x in xs]
     all(y -> y==symbols[1], symbols) || @error "cannot concatenate feature data with different keys"
-    length(xs) == 1 && return xs[1] 
+    length(xs) == 1 && return xs[1]
 
-    # concatenate 
+    # concatenate
     syms = symbols[1]
     NamedTuple(
         k => cat_features([x[k] for x in xs]) for (ii,k) in enumerate(syms)
@@ -71,8 +71,8 @@ end
 # Turns generic type into named tuple
 normalize_graphdata(data::Nothing; kws...) = NamedTuple()
 
-normalize_graphdata(data; default_name::Symbol, kws...) = 
-    normalize_graphdata(NamedTuple{(default_name,)}((data,)); default_name, kws...) 
+normalize_graphdata(data; default_name::Symbol, kws...) =
+    normalize_graphdata(NamedTuple{(default_name,)}((data,)); default_name, kws...)
 
 function normalize_graphdata(data::NamedTuple; default_name, n, duplicate_if_needed=false)
     # This had to workaround two Zygote bugs with NamedTuples
@@ -82,23 +82,23 @@ function normalize_graphdata(data::NamedTuple; default_name, n, duplicate_if_nee
     if n != 1
         @assert all(x -> x isa AbstractArray, data) "Non-array features provided."
     end
-    
+
     if n == 1
-        # If last array dimension is not 1, add a new dimension. 
+        # If last array dimension is not 1, add a new dimension.
         # This is mostly useful to reshape global feature vectors
         # of size D to Dx1 matrices.
         unsqz_last(v::AbstractArray) = size(v)[end] != 1 ? reshape(v, size(v)..., 1) : v
         unsqz_last(v) = v
-    
+
         data = map(unsqz_last, data)
     end
-    
-    ## Turn vectors in 1 x n matrices. 
+
+    ## Turn vectors in 1 x n matrices.
     # unsqz_first(v::AbstractVector) = reshape(v, 1, length(v))
     # unsqz_first(v) = v
     # data = map(unsqz_first, data)
-    
-    if duplicate_if_needed 
+
+    if duplicate_if_needed
         function duplicate(v)
             if v isa AbstractArray && size(v)[end] == n÷2
                 v = cat(v, v, dims=ndims(v))
@@ -107,12 +107,12 @@ function normalize_graphdata(data::NamedTuple; default_name, n, duplicate_if_nee
         end
         data = map(duplicate, data)
     end
-    
+
     for x in data
         if x isa AbstractArray
             @assert size(x)[end] == n "Wrong size in last dimension for feature array, expected $n but got $(size(x)[end])."
         end
-    end    
+    end
     return data
 end
 
@@ -141,10 +141,10 @@ function edge_encoding(s, t, n; directed=true)
         # directed edges and self-loops allowed
         idx = (s .- 1) .* n .+ t
         maxid = n^2
-    else 
+    else
         # Undirected edges and self-loops allowed
         maxid = n * (n + 1) ÷ 2
-        
+
         mask = s .> t
         snew = copy(s)
         tnew = copy(t)
@@ -152,7 +152,7 @@ function edge_encoding(s, t, n; directed=true)
         tnew[mask] .= s[mask]
         s, t = snew, tnew
 
-        # idx = ∑_{i',i'<i} ∑_{j',j'>=i'}^n 1 + ∑_{j',i<=j'<=j} 1 
+        # idx = ∑_{i',i'<i} ∑_{j',j'>=i'}^n 1 + ∑_{j',i<=j'<=j} 1
         #     = ∑_{i',i'<i} ∑_{j',j'>=i'}^n 1 + (j - i + 1)
         #     = ∑_{i',i'<i} (n - i' + 1) + (j - i + 1)
         #     = (i - 1)*(2*(n+1)-i)÷2 + (j - i + 1)
@@ -169,14 +169,14 @@ function edge_decoding(idx, n; directed=true)
         s =  (idx .- 1) .÷ n .+ 1
         t =  (idx .- 1) .% n .+ 1
     else
-        # We replace j=n in 
-        # idx = (i - 1)*(2*(n+1)-i)÷2 + (j - i + 1) 
+        # We replace j=n in
+        # idx = (i - 1)*(2*(n+1)-i)÷2 + (j - i + 1)
         # and obtain
-        # idx = (i - 1)*(2*(n+1)-i)÷2 + (n - i + 1) 
-        
+        # idx = (i - 1)*(2*(n+1)-i)÷2 + (n - i + 1)
+
         # OR We replace j=i  and obtain??
-        # idx = (i - 1)*(2*(n+1)-i)÷2 + 1 
-        
+        # idx = (i - 1)*(2*(n+1)-i)÷2 + 1
+
         # inverting we have
         s = @. ceil(Int, -sqrt((n + 1/2)^2 - 2*idx) + n + 1/2)
         t = @. idx - (s-1)*(2*(n+1)-s)÷2 - 1 + s
@@ -213,7 +213,7 @@ end
 shortsummary(x) = summary(x)
 shortsummary(x::Number) = "$x"
 
-function shortsummary(x::NamedTuple) 
+function shortsummary(x::NamedTuple)
     if length(x) == 0
         return nothing
     elseif length(x) === 1
