5e87a5bd38184007488481c528e49e2f1f6e25f4
aurorarossi <aurora98rossi@gmail.com>
2022-12-07 11:28:14 +0100

    Add more tests

--- test/GNNGraphs/query.jl
+++ test/GNNGraphs/query.jl
@@ -170,9 +170,12 @@
             t = [2,3,1]
             w = [0.1,0.1,0.2]
             g = GNNGraph(s, t, w)
-            @test khop_adj(g,2) == Float32.([0.0 0.0 0.01;0.02 0.0 0.0;0.0 0.02 0.0])
-            @test khop_adj(g,1) == Float32.(Matrix(adjacency_matrix(g)))
-            @test eltype(khop_adj(g,4)) == Float32    
+            @test khop_adj(g,2)== adjacency_matrix(g)*adjacency_matrix(g)
+            @test khop_adj(g,2,Int8;weighted=false) == sparse([0 0 1;1 0 0;0 1 0])
+            @test khop_adj(g,2,Int8;dir=in,weighted=false) == sparse([0 0 1;1 0 0;0 1 0]')
+            @test khop_adj(g,1) == adjacency_matrix(g)
+            @test eltype(khop_adj(g,4)) == Float64
+            @test eltype(khop_adj(g,10,Float32)) == Float32    
         end
     end
 end
