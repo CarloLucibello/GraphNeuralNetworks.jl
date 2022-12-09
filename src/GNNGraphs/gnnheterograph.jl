5ae657d38edecfc5833b497f73895473a5aac7d8
Aurora Rossi <65721467+aurorarossi@users.noreply.github.com>
2022-12-09 10:08:44 +0100

    Fix gnnheterographs failed test (#241)
    * Fix print order for Dict
    
    * Add sorted order to dict print
    
    * Fix style
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>
    
    * Add spaces
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>
    
    * Fix indentations
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- src/GNNGraphs/gnnheterograph.jl
+++ src/GNNGraphs/gnnheterograph.jl
@@ -130,26 +130,58 @@ function GNNHeteroGraph(data::EDict;
         ntypes, etypes)
 end
 
+function show_sorted_Dict(io::IO, d::Dict, compact::Bool)
+    if compact
+        print(io, "Dict")
+    end
+    print(io, "(")
+    if !isempty(d)
+        if length(keys(d)) == 1
+            show(io, keys[1])
+            print(io, " => $(d[keys[1]])")
+        else
+            sorted_keys = sort!(collect(keys(d)))
+            for key in sorted_keys[1:end-1]
+                show(io, key)
+                print(io, " => $(d[key]), ")
+            end
+            show(io, sorted_keys[end])
+            print(io, " => $(d[sorted_keys[end]])")
+        end
+    end
+    print(io, ")")
+end
 
 function Base.show(io::IO, g::GNNHeteroGraph)
-    print(io, "GNNHeteroGraph($(g.num_nodes), $(g.num_edges))")
+    print(io, "GNNHeteroGraph(")
+    show_sorted_Dict(io, g.num_nodes, true)
+    print(io, ", ")
+    show_sorted_Dict(io, g.num_edges, true)
+    print(io, ")")
 end
 
 function Base.show(io::IO, ::MIME"text/plain", g::GNNHeteroGraph)
     if get(io, :compact, false)
-        print(io, "GNNHeteroGraph($(g.num_nodes), $(g.num_edges))")
+        print(io, "GNNHeteroGraph(")
+        show_sorted_Dict(io, g.num_nodes, true)
+        print(io, ", ")
+        show_sorted_Dict(io, g.num_edges, true)
+        print(io, ")")
     else
-        print(io, "GNNHeteroGraph:\n num_nodes: $((g.num_nodes...,))\n num_edges: $((g.num_edges...,))")
+        print(io, "GNNHeteroGraph:\n num_nodes: ")
+        show_sorted_Dict(io, g.num_nodes, false)
+        print(io, "\n num_edges: ")
+        show_sorted_Dict(io, g.num_edges, false)
         g.num_graphs > 1 && print(io, "\n num_graphs: $(g.num_graphs)")
         if !isempty(g.ndata)
             print(io, "\n ndata:")
-            for k in keys(g.ndata)
+            for k in sort(collect(keys(g.ndata)))
                 print(io, "\n\t", _str(k), "  =>  $(shortsummary(g.ndata[k]))")
             end
         end
         if !isempty(g.edata)
             print(io, "\n edata:")
-            for k in keys(g.edata)
+            for k in sort(collect(keys(g.edata)))
                 print(io, "\n\t$k  =>  $(shortsummary(g.edata[k]))")
             end
         end
