9f81f7089672288a51f812df06bee6b6d9b96006
CarloLucibello <carlo.lucibello@gmail.com>
2023-03-19 08:58:12 +0100

    cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -47,7 +47,7 @@ tests = [
 !CUDA.functional() && @warn("CUDA unavailable, not testing GPU support")
 
 # @testset "GraphNeuralNetworks: graph format $graph_type" for graph_type in (:coo, :dense, :sparse)
-# for graph_type in (:coo, :dense, :sparse)
+for graph_type in (:coo, :dense, :sparse)
     @info "Testing graph format :$graph_type"
     # global GRAPH_T = graph_type
     # global TEST_GPU = CUDA.functional() && (GRAPH_T != :sparse)
@@ -59,4 +59,4 @@ tests = [
         t == "GNNGraphs/sampling" && GRAPH_T != :coo && continue
         include("$t.jl")
     end
-# end
+end
