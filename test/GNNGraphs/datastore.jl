f03db808e7601fd8b69912e52b05387b48f2dfdf
aurorarossi <aurora98rossi@gmail.com>
2023-06-12 14:16:54 +0200

    Add test `getproperty`

--- test/GNNGraphs/datastore.jl
+++ test/GNNGraphs/datastore.jl
@@ -20,6 +20,9 @@ end
     @test_throws DimensionMismatch ds.z=rand(12)
     ds.z = [1:10;]
     @test ds.z == [1:10;]
+    vec = [DataStore(10, (:x => x,)), DataStore(10, (:x => x, :y => rand(2, 10)))]
+    @test vec.x == [x, x]
+    @test_throws KeyError vec.z 
 end
 
 @testset "map" begin
