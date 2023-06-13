9c025c80e0e4706d298336b47e26c6128b129a41
aurorarossi <aurora98rossi@gmail.com>
2023-06-13 12:58:25 +0200

    Add more test `Datastore`

--- test/GNNGraphs/datastore.jl
+++ test/GNNGraphs/datastore.jl
@@ -23,6 +23,8 @@ end
     vec = [DataStore(10, (:x => x,)), DataStore(10, (:x => x, :y => rand(2, 10)))]
     @test vec.x == [x, x]
     @test_throws KeyError vec.z 
+    @test vec._n == [10, 10]
+    @test vec._data == [Dict(:x => x), Dict(:x => x, :y => vec[2].y)]
 end
 
 @testset "map" begin
@@ -34,7 +36,7 @@ end
     @test_throws AssertionError ds2=map(x -> [x; x], ds)
 end
 
-@testset """getdata / getn""" begin
+@testset "getdata / getn" begin
     ds = DataStore(10, (:x => rand(10), :y => rand(2, 10)))
     @test getdata(ds) == getfield(ds, :_data)
     @test_throws KeyError ds.data
