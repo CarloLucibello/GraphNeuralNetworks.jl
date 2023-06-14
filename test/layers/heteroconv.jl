cd62f7adb7982cdd769d7bc57097bbb0eea07f10
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-06-14 22:10:00 +0200

    more tests

--- test/layers/heteroconv.jl
+++ test/layers/heteroconv.jl
@@ -5,17 +5,26 @@
     model = HeteroGraphConv([(:A,:to,:B) => GraphConv(d => d), 
                             (:B,:to,:A) => GraphConv(d => d)])
 
-    x = (A = rand(Float32, d, n), B = rand(Float32, d, 2n))
+    for x in [
+                (A = rand(Float32, d, n), B = rand(Float32, d, 2n)),
+                Dict(:A => rand(Float32, d, n), :B => rand(Float32, d, 2n)) 
+             ]
+        # x = (A = rand(Float32, d, n), B = rand(Float32, d, 2n))
+        x = Dict(:A => rand(Float32, d, n), :B => rand(Float32, d, 2n)) 
+      
+        y = model(g, x)
 
-    y = model(g, x)
+        grad, dx = gradient((model, x) -> sum(model(g, x)[1]) + sum(model(g, x)[2].^2), model, x)
+        ngrad, ndx = ngradient((model, x) -> sum(model(g, x)[1]) + sum(model(g, x)[2].^2), model, x)
 
-    grad = gradient(model -> sum(model(g, x)[1]) + sum(model(g, x)[2].^2), model)[1]
-    ngrad = ngradient(model -> sum(model(g, x)[1]) + sum(model(g, x)[2].^2), model)[1]
+        @test grad.layers[1].weight1 ≈ ngrad.layers[1].weight1  rtol=1e-4
+        @test grad.layers[1].weight2 ≈ ngrad.layers[1].weight2  rtol=1e-4
+        @test grad.layers[1].bias ≈ ngrad.layers[1].bias        rtol=1e-4
+        @test grad.layers[2].weight1 ≈ ngrad.layers[2].weight1  rtol=1e-4
+        @test grad.layers[2].weight2 ≈ ngrad.layers[2].weight2  rtol=1e-4
+        @test grad.layers[2].bias ≈ ngrad.layers[2].bias        rtol=1e-4
 
-    @test grad.layers[1].weight1 ≈ ngrad.layers[1].weight1  rtol=1e-4
-    @test grad.layers[1].weight2 ≈ ngrad.layers[1].weight2  rtol=1e-4
-    @test grad.layers[1].bias ≈ ngrad.layers[1].bias        rtol=1e-4
-    @test grad.layers[2].weight1 ≈ ngrad.layers[2].weight1  rtol=1e-4
-    @test grad.layers[2].weight2 ≈ ngrad.layers[2].weight2  rtol=1e-4
-    @test grad.layers[2].bias ≈ ngrad.layers[2].bias        rtol=1e-4
+        @test dx[:A] ≈ ndx[:A]        rtol=1e-4
+        @test dx[:B] ≈ ndx[:B]        rtol=1e-4
+    end
 end
