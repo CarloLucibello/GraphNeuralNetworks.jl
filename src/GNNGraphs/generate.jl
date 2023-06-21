5a9d26bce628da89cb6701eb9213211c16802b59
aurorarossi <aurora98rossi@gmail.com>
2023-06-21 12:45:59 +0200

    Add docstring

--- src/GNNGraphs/generate.jl
+++ src/GNNGraphs/generate.jl
@@ -373,6 +373,47 @@ function _hyperbolic_distance(nodeA::Array{Float64, 1},nodeB::Array{Float64, 1};
     return d
 end
 
+"""
+    rand_temporal_hyperbolic_graph(number_nodes::Int, 
+                                   number_snapshots::Int;
+                                   α::Real,
+                                   R::Real,
+                                   speed::Real,
+                                   ζ::Real=1,
+                                   self_loop = false,
+                                   kws...)
+
+Create a random temporal graph given `number_nodes` nodes and `number_snapshots` snapshots.
+First, the positions of the nodes are generated with a quasi-uniform distribution (depending on the parameter `α`) in hyperbolic space within a disk of radius `R`. Two nodes are connected if their hyperbolic distance is less than `R`. Each following snapshot is created in order to keep the same initial distribution.
+
+# Arguments
+
+- `number_nodes`: The number of nodes of each snapshot.
+- `number_snapshots`: The number of snapshots.
+- `α`: The parameter that controls the position of the points. If `α=ζ`, the points are uniformly distributed on the disk of radius `R`. If `α>ζ`, the points are more concentrated in the center of the disk. If `α<ζ`, the points are more concentrated at the boundary of the disk.
+- `R`: The radius of the disk and of connection.
+- `speed`: The speed to update the nodes.
+- `ζ`: The parameter that controls the curvature of the disk.
+- `self_loops`: If `true`, consider the node itself among its neighbors, in which
+                case the graph will contain self-loops.
+- `kws`: Further keyword arguments will be passed to the [`GNNGraph`](@ref) constructor of each snapshot.
+
+# Example
+
+```julia-repl
+julia> n, snaps, α, R, speed, ζ = 10, 5, 1.0, 4.0, 0.1, 1.0;
+
+julia> thg = rand_temporal_hyperbolic_graph(n, snaps; α, R, speed, ζ)
+TemporalSnapshotsGNNGraph:
+  num_nodes: [10, 10, 10, 10, 10]
+  num_edges: [44, 46, 48, 42, 38]
+  num_snapshots: 5
+```
+
+# References
+Section D of the paper [Dynamic Hidden-Variable Network Models](https://arxiv.org/pdf/2101.00414.pdf) and the paper 
+[Hyperbolic Geometry of Complex Networks](https://arxiv.org/pdf/1006.5169.pdf)
+"""
 function rand_temporal_hyperbolic_graph(number_nodes::Int,
                                         number_snapshots::Int;
                                         α::Real,
@@ -384,9 +425,9 @@ function rand_temporal_hyperbolic_graph(number_nodes::Int,
         @assert number_snapshots > 1 "The number of snapshots must be greater than 1"
         @assert α > 0 "α must be greater than 0"
 
-        probabilities=rand(number_nodes)
+        probabilities = rand(number_nodes)
 
-        points=Array{Float64}(undef,2,number_nodes)
+        points = Array{Float64}(undef,2,number_nodes)
         points[1,:].= (1/α)*acosh.(1 .+ (cosh(α * R) - 1)*probabilities)
         points[2,:].= 2*pi*rand(number_nodes)
 
@@ -404,6 +445,7 @@ function rand_temporal_hyperbolic_graph(number_nodes::Int,
                 end
             end
             tg[time] = GNNGraph(adj)
+            
             probabilities.=probabilities.+ (2 * speed * rand(number_nodes) .- speed)
             probabilities[probabilities.>1].= 1 .- (probabilities[probabilities.>1].%1)
             probabilities[probabilities.<0].= abs.(probabilities[probabilities.<0])
