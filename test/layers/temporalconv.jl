176f8a2f11815bc34546421ba20214bed789092f
aurorarossi <aurora98rossi@gmail.com>
2023-07-25 21:21:18 +0200

    Add test

--- test/layers/temporalconv.jl
+++ test/layers/temporalconv.jl
@@ -8,9 +8,14 @@ g1 = GNNGraph(rand_graph(N,8),
                 graph_type = :sparse)
 
 @testset "TGCNCell" begin
-    tgcn = TGCNCell(in_channel => out_channel)
+    tgcn = GraphNeuralNetworks.TGCNCell(in_channel => out_channel)
     h, x̃ = tgcn(tgcn.state0, g1, g1.ndata.x)
     @test size(h) == (out_channel, N)
     @test size(x̃) == (out_channel, N)
     @test h == x̃
+end
+
+@testset "TGCN" begin
+    tgcn = GraphNeuralNetworks.TGCN(in_channel => out_channel)
+    @test size(Flux.gradient(x -> sum(tgcn(g1, x)), g1.ndata.x)[1]) == (in_channel, N)
 end
