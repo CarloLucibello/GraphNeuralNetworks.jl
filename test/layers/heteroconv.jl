e7eab729e952634dd51ee36baad923185369cb0a
Agata Skorupka <agata.skorupka@gmail.com>
2024-01-28 22:10:09 +0100

    add test

--- test/layers/heteroconv.jl
+++ test/layers/heteroconv.jl
@@ -155,5 +155,12 @@
                                  (:B, :to, :A) => GATv2Conv(4 => 2));
         y = layers(hg, x); 
         @test size(y.A) == (2, 2) && size(y.B) == (2, 3)
+    @testset "GCNConv" begin
+        g = rand_bipartite_heterograph((2,3), 6)
+        x = (A = rand(Float32, 4,2), B = rand(Float32, 4, 3))
+        layers = HeteroGraphConv( (:A, :to, :B) => GCNConv(4 => 2, relu),
+                                    (:B, :to, :A) => GCNConv(4 => 2, relu));
+        y = layers(g, x); 
+        @test size(y.A) == (2,2) && size(y.B) == (2,3)
     end
 end
