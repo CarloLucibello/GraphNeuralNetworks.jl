664e682d2daf2eaf2143357ec937ca5845592ef4
rbSparky <acompsciproject@gmail.com>
2024-02-21 18:00:34 +0530

    SAGEConv Hetero Layer

--- src/layers/conv.jl
+++ src/layers/conv.jl
@@ -801,9 +801,10 @@ function SAGEConv(ch::Pair{Int, Int}, σ = identity; aggr = mean,
     SAGEConv(W, b, σ, aggr)
 end
 
-function (l::SAGEConv)(g::GNNGraph, x::AbstractMatrix)
+function (l::SAGEConv)(g::AbstractGNNGraph, x)
     check_num_nodes(g, x)
-    m = propagate(copy_xj, g, l.aggr, xj = x)
+    xj, _ = expand_srcdst(g, x)
+    m = propagate(copy_xj, g, l.aggr, xj = xj)
     x = l.σ.(l.weight * vcat(x, m) .+ l.bias)
     return x
 end
--- test/layers/heteroconv.jl
+++ test/layers/heteroconv.jl
@@ -109,4 +109,12 @@
         @test size(y.A) == (2,2) && size(y.B) == (2,3)
     end
 
+    @testset "SAGEConv" begin
+        x = (A = rand(Float32, 4, 2), B = rand(Float32, 4, 3))
+        layers = HeteroGraphConv((:A, :to, :B) => SAGEConv(4 => 2, relu),
+                                 (:B, :to, :A) => SAGEConv(4 => 2, relu));
+        y = layers(hg, x); 
+        @test size(y.A) == (2, 2) && size(y.B) == (2, 3)
+    end
+
 end
