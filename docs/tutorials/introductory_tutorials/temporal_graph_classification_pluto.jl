46dfaf98611e9badfbd5ef1a88d7035086f5aff8
Aurora Rossi <aurora98rossi@gmail.com>
2024-03-04 18:12:34 +0100

    Create file

---
+++ docs/tutorials/introductory_tutorials/temporal_graph_classification_pluto.jl
@@ -0,0 +1,12 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 69d00ec8-da47-11ee-1bba-13a14e8a6db2
+md"In this tutorial, we learn how to extend the graph classification task to the case of temporal graphs, which are graphs whose topology and features are time-varying.
+"
+
+# ╔═╡ Cell order:
+# ╠═69d00ec8-da47-11ee-1bba-13a14e8a6db2
