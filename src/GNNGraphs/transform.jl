0951acc75be6263e3048a44e7abecf51fbaf2191
Rishabh <59335537+rbSparky@users.noreply.github.com>
2024-03-06 01:47:24 +0530

    Update transform.jl

--- src/GNNGraphs/transform.jl
+++ src/GNNGraphs/transform.jl
@@ -53,8 +53,6 @@ end
 
 - For the entire graph (without specifying `edge_t`):
     Adds a self-loop to each node for every edge type in the graph where the source and destination node types are the same. This variant of the function iterates over all edge types present in the graph, applying the self-loop addition logic to each applicable edge type.
-
-The first variant of the function targets a specific edge type, enhancing fine-grained control over graph modification, while the second variant applies this enhancement across all edge types in the graph, offering a broad and automatic way to enrich the graph's structure.
 """
 function add_self_loops(g::GNNHeteroGraph{Tuple{T, T, V}}, edge_t::EType) where {T <: AbstractVector{<:Integer}, V}
     function get_edge_weight_nullable(g::GNNHeteroGraph{<:COO_T}, edge_t::EType)
