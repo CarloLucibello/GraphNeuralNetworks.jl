e8f11cebca1d8626cbf35fd43bab1a9bc0895b24
rbSparky <rishb369@gmail.com>
2024-03-06 13:26:48 +0530

    docs

--- src/GNNGraphs/transform.jl
+++ src/GNNGraphs/transform.jl
@@ -42,16 +42,16 @@ end
     add_self_loops(g::GNNHeteroGraph, edge_t::EType)
     add_self_loops(g::GNNHeteroGraph)
 
-If the source node type is the same as destination node type in `edge_t`,
-return a graph with the same features as `g` but also adding self-loops 
-of the specified type, `edge_t`. Otherwise it returns `g` unchanged.
+If the source node type is the same as the destination node type in `edge_t`,
+return a graph with the same features as `g` but also add self-loops 
+of the specified type, `edge_t`. Otherwise, it returns `g` unchanged.
 
-Nodes with already existing self-loops of type edge_t will obtain 
-a second self-loop of type `edge_t``.
+Nodes with already existing self-loops of type `edge_t` will obtain 
+a second set of self-loops of the same type.
 
-If the graphs has edge weights for edges of type `edge_t``, the new edges will have weight 1.
+If the graph has edge weights for edges of type `edge_t`, the new edges will have weight 1.
 
-If no edges of type `edge_t`` exist, or all existing edges have no weight, 
+If no edges of type `edge_t` exist, or all existing edges have no weight, 
 then all new self loops will have no weight.
 
 If `edge_t` is not passed as argument, for the entire graph self-loop is added to each node for every edge type in the graph where the source and destination node types are the same. 
