5d21df383ec9bc51a2bf65b805f59700332bfc9d
Agata Skorupka <agata.skorupka@gmail.com>
2024-03-09 23:19:45 +0100

    fix: dont remove SimpleWeightedGraph extension

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,7 @@ using Zygote
 using Test
 using MLDatasets
 using InlineStrings  # not used but with the import we test #98 and #104
+using SimpleWeightedGraphs
 
 CUDA.allowscalar(false)
 
@@ -46,6 +47,7 @@ tests = [
     "mldatasets",
     "examples/node_classification_cora",
     "deprecations",
+    "ext/GraphNeuralNetworksSimpleWeightedGraphsExt/GraphNeuralNetworksSimpleWeightedGraphsExt"
 ]
 
 !CUDA.functional() && @warn("CUDA unavailable, not testing GPU support")
