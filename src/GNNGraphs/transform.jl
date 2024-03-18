aece851dd795d1ac2b7d143ba8230f3a1fd1425e
rbSparky <rishb369@gmail.com>
2024-03-19 02:41:03 +0530

    add ppr diffusion

--- src/GNNGraphs/transform.jl
+++ src/GNNGraphs/transform.jl
@@ -1078,3 +1078,53 @@ ci2t(ci::AbstractVector{<:CartesianIndex}, dims) = ntuple(i -> map(x -> x[i], ci
 @non_differentiable remove_self_loops(x...)  # TODO this is wrong, since g carries feature arrays, needs rrule
 @non_differentiable dense_zeros_like(x...)
 
+"""
+    ppr_diffusion(g::GNNGraph; alpha_f32::Float32=0.85f0) -> GNNGraph
+
+Calculates the Personalized PageRank (PPR) diffusion based on the edge weight matrix of a GNNGraph and updates the graph with new edge weights derived from the PPR matrix.
+
+The function performs the following steps:
+1. Constructs a modified adjacency matrix `A` using the graph's edge weights, where `A` is adjusted by `(α - 1) * A + I`, with `α` being the damping factor (`alpha_f32`) and `I` the identity matrix.
+2. Normalizes `A` to ensure each column sums to 1, representing transition probabilities.
+3. Applies the PPR formula `α * (I + (α - 1) * A)^-1` to compute the diffusion matrix.
+4. Updates the original edge weights of the graph based on the PPR diffusion matrix, assigning new weights for each edge from the PPR matrix.
+
+# Arguments
+- `g::GNNGraph`: The input graph for which PPR diffusion is to be calculated. It should have edge weights available.
+- `alpha_f32::Float32`: The damping factor used in PPR calculation, controlling the teleport probability in the random walk. Defaults to `0.85f0`.
+
+# Returns
+- A new `GNNGraph` instance with the same structure as `g` but with updated edge weights according to the PPR diffusion calculation.
+"""
+function ppr_diffusion(g::GNNGraph; alpha_f32::Float32=0.85f0)
+    s, t = edge_index(g)
+    w = get_edge_weight(g)  
+
+    N = g.num_nodes
+
+    A = zeros(Float32, N, N)
+    
+    for (idx, src, dst) in zip(1:length(s), s, t)
+        A[dst, src] += w[idx]  
+    end
+
+    A .*= (alpha_f32 - 1)
+
+    for i in 1:N
+        A[i, i] += 1
+    end
+
+    # α * (I + (α - 1) * A)^-1
+    PPR = alpha_f32 * inv(A)
+    println(PPR)
+
+    new_w = Float32[]
+    for (src, dst) in zip(s, t)
+        push!(new_w, PPR[dst, src]) 
+    end
+
+    return GNNGraph((s, t, new_w),
+                    g.num_nodes, length(s), g.num_graphs,
+                    g.graph_indicator,
+                    g.ndata, g.edata, g.gdata)
+end
