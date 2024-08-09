62b3405ec2c0edf8180958ba44bd5037ba06fe66
Rishabh <59335537+rbSparky@users.noreply.github.com>
2024-08-09 15:10:34 +0530

    Update basic_tests.jl

--- GNNLux/test/layers/basic_tests.jl
+++ GNNLux/test/layers/basic_tests.jl
@@ -1,6 +1,4 @@
 @testitem "layers/basic" setup=[SharedTestSetup] begin
-    @test 1==1
-    """
     rng = StableRNG(17)
     g = rand_graph(10, 40, seed=17)
     x = randn(rng, Float32, 3, 10)        
@@ -18,5 +16,4 @@
         c = GNNChain(GraphConv(3 => 5, relu), GCNConv(5 => 3))
         test_lux_layer(rng, c, g, x, outputsize=(3,), container=true)
     end
-    """
 end
