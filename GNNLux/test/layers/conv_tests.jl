91fed90cfa565f496059fde033078e9ecfbdd239
Rishabh <59335537+rbSparky@users.noreply.github.com>
2024-09-04 16:20:35 +0530

    Update conv_tests.jl: checking test

--- GNNLux/test/layers/conv_tests.jl
+++ GNNLux/test/layers/conv_tests.jl
@@ -97,11 +97,26 @@
         test_lux_layer(rng, l, g, x, sizey=(out_dims,g.num_nodes), container=true)
     end
 
+     
+
     @testset "NNConv" begin
-        edim = 10
-        nn = Dense(edim, in_dims * out_dims)        
-        l = NNConv(in_dims => out_dims, nn, tanh, aggr = +)
-        test_lux_layer(rng, l, g2, x, sizey=(out_dims, g2.num_nodes), container=true, edge_weight=g2.edata.e) 
+        n_in = 3
+        n_in_edge = 10
+        n_out = 5
+        
+        s = [1,1,2,3]
+        t = [2,3,1,1]
+        g2 = GNNGraph(s, t)
+        
+        nn = Dense(n_in_edge => n_out * n_in)
+        l = NNConv(n_in => n_out, nn, tanh, aggr = +)
+        x = randn(Float32, n_in, g2.num_nodes)
+        e = randn(Float32, n_in_edge, g2.num_edges)
+        y = l(g, x, e) # just to see if it runs without an error
+        #edim = 10
+        #nn = Dense(edim, in_dims * out_dims)        
+        #l = NNConv(in_dims => out_dims, nn, tanh, aggr = +)
+        #test_lux_layer(rng, l, g2, x, sizey=(out_dims, g2.num_nodes), container=true, edge_weight=g2.edata.e) 
     end          
         
     @testset "MEGNetConv" begin
