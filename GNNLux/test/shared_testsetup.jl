73de0db1e71f672bb9c8fe3d03ed8974f04d5533
rbSparky <rishb369@gmail.com>
2024-09-14 22:56:48 +0530

    nnlux

--- GNNLux/test/shared_testsetup.jl
+++ GNNLux/test/shared_testsetup.jl
@@ -14,7 +14,7 @@ export test_lux_layer
 
 function test_lux_layer(rng::AbstractRNG, l, g::GNNGraph, x; 
             outputsize=nothing, sizey=nothing, container=false,
-            atol=1.0f-2, rtol=1.0f-2)
+            atol=1.0f-2, rtol=1.0f-2, edge_weight=nothing) 
 
     if container
         @test l isa GNNContainerLayer
@@ -27,7 +27,11 @@ function test_lux_layer(rng::AbstractRNG, l, g::GNNGraph, x;
     @test LuxCore.parameterlength(l) == LuxCore.parameterlength(ps)
     @test LuxCore.statelength(l) == LuxCore.statelength(st)
     
-    y, st′ = l(g, x, ps, st)
+    if edge_weight !== nothing
+        y, st′ = l(g, x, edge_weight, ps, st)
+    else          
+        y, st′ = l(g, x, ps, st)
+    end
     @test eltype(y) == eltype(x)
     if outputsize !== nothing
         @test LuxCore.outputsize(l) == outputsize
