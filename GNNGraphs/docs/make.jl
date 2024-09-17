a467be7edb102d97e5995f93d9123ea05bfdc522
Aurora Rossi <aurora98rossi@gmail.com>
2024-09-17 16:48:22 +0200

    First try

---
+++ GNNGraphs/docs/make.jl
@@ -0,0 +1,36 @@
+using Documenter
+using GNNGraphs
+using Graphs
+
+assets=[]
+prettyurls = get(ENV, "CI", nothing) == "true"
+mathengine = MathJax3()
+
+makedocs(;
+         modules = [GNNGraphs],
+         doctest = false,
+         clean = true,
+         format = Documenter.HTML(; mathengine, prettyurls, assets = assets, size_threshold=nothing),
+         sitename = "GNNGraphs.jl",
+         pages = [#"Home" => "index.md",
+            "Graphs" => ["gnngraph.md", "heterograph.md", "temporalgraph.md"],
+            #  "Message Passing" => "messagepassing.md",
+            #  "Model Building" => "models.md",
+            #  "Datasets" => "datasets.md",
+            #  #"Tutorials" => tutorials,
+            "API Reference" => [
+                 "GNNGraph" => "api/gnngraph.md",
+            #      "Basic Layers" => "api/basic.md",
+            #      "Convolutional Layers" => "api/conv.md",
+            #      "Pooling Layers" => "api/pool.md",
+            #      "Message Passing" => "api/messagepassing.md",
+                 "Heterogeneous Graphs" => "api/heterograph.md",
+                 "Temporal Graphs" => "api/temporalgraph.md",
+            #      "Utils" => "api/utils.md",
+              ],
+            #  "Developer Notes" => "dev.md",
+            
+         ])
+
+
+deploydocs(repo = "github.com/CarloLucibello/GraphNeuralNetworks/GNNGraphs.jl.git")
---
+++ GNNGraphs/docs/src/api/temporalgraph.md
@@ -0,0 +1,18 @@
+# Temporal Graphs
+
+## TemporalSnapshotsGNNGraph
+
+Documentation page for the graph type `TemporalSnapshotsGNNGraph` and related methods, representing time varying graphs with time varying features.
+
+```@autodocs
+Modules = [GNNGraphs]
+Pages   = ["temporalsnapshotsgnngraph.jl"]
+Private = false
+```
+
+### TemporalSnapshotsGNNGraph random generators
+
+```@docs
+rand_temporal_radius_graph
+rand_temporal_hyperbolic_graph
+```
