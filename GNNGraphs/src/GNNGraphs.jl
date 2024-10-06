2f528aa5542c30fb98eb1e4217340d95c35cdae2
Agata Skorupka <agata.skorupka@gmail.com>
2024-10-06 22:39:17 +0200

    fix: revert naming for induced_subgraph

--- GNNGraphs/src/GNNGraphs.jl
+++ GNNGraphs/src/GNNGraphs.jl
@@ -96,7 +96,7 @@ export rand_graph,
        rand_temporal_hyperbolic_graph
 
 include("sampling.jl")
-export sample_neighbors, Graphs.induced_subgraph
+export sample_neighbors, induced_subgraph
 
 include("operators.jl")
 # Base.intersect
