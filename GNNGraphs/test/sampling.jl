8dbac1712dd1835216ae2e6002dcc6e7fe8b9d83
Agata Skorupka <45850123+askorupka@users.noreply.github.com>
2024-10-12 11:03:37 +0200

    feat: add induced_subgraph functionality (#499)
    * feat: add induced_subgraph functionality
    
    * fix: fix tests
    
    * fix: fix tests
    
    * Update GNNGraphs/src/sampling.jl
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>
    
    * Update GNNGraphs/src/GNNGraphs.jl
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>
    
    * Update GNNGraphs/src/sampling.jl
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>
    
    * Update GNNGraphs/src/sampling.jl
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>
    
    * feat: add edata&ndata support for induced_subgraph
    
    * chore: export induced_subgraph
    
    * fix: fix naming for induced_subgraph
    
    * fix: fix typo
    
    * fix: revert naming for induced_subgraph
    
    * fix: fix test
    
    * fix: don't export induced subgraph
    
    * fix: don't export induced subgraph
    
    * fix: amend docstring
    
    * fix: amend docstring
    
    * fix: fix docstring
    
    * fix: add Graphs.induced_subgraph to docs
    
    ---------
    
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- GNNGraphs/test/sampling.jl
+++ GNNGraphs/test/sampling.jl
@@ -45,4 +45,36 @@ if GRAPH_T == :coo
         @test sg.ndata.x1 == g.ndata.x1[sg.ndata.NID]
         @test length(union(sg.ndata.NID)) == length(sg.ndata.NID)
     end
+
+    @testset "induced_subgraph" begin
+        s = [1, 2]
+        t = [2, 3]
+        
+        graph = GNNGraph((s, t), ndata = (; x=rand(Float32, 32, 3), y=rand(Float32, 3)), edata = rand(Float32, 2))
+        
+        nodes = [1, 2, 3]
+        subgraph = Graphs.induced_subgraph(graph, nodes)
+        
+        @test subgraph.num_nodes == 3  
+        @test subgraph.num_edges == 2  
+        @test subgraph.ndata.x == graph.ndata.x
+        @test subgraph.ndata.y == graph.ndata.y
+        @test subgraph.edata == graph.edata
+        
+        nodes = [1, 2]
+        subgraph = Graphs.induced_subgraph(graph, nodes)
+
+        @test subgraph.num_nodes == 2 
+        @test subgraph.num_edges == 1 
+        @test subgraph.ndata == getobs(graph.ndata, [1, 2])
+        @test isapprox(getobs(subgraph.edata.e, 1), getobs(graph.edata.e, 1); atol=1e-6)
+
+        graph = GNNGraph(2)
+        graph = add_edges(graph, ([2], [1]))
+        nodes = [1]
+        subgraph = Graphs.induced_subgraph(graph, nodes)
+        
+        @test subgraph.num_nodes == 1 
+        @test subgraph.num_edges == 0 
+    end
 end
