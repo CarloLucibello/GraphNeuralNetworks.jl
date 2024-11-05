28ba5ac2dfd954a43fcf399b6077efc6d0dac606
Aurora Rossi <aurora98rossi@gmail.com>
2024-11-05 15:14:47 +0100

    Fix workflows for PR

--- docs/make-multi.jl
+++ docs/make-multi.jl
@@ -55,52 +55,54 @@ MultiDocumenter.make(
 cp(joinpath(@__DIR__, "logo.svg"),
     joinpath(outpath, "logo.svg"))
 
-@warn "Deploying to GitHub as MultiDocumenter" 
-gitroot = normpath(joinpath(@__DIR__, ".."))
-run(`git pull`)
+if "PR" not in ARGS
+    @warn "Deploying to GitHub as MultiDocumenter" 
+    gitroot = normpath(joinpath(@__DIR__, ".."))
+    run(`git pull`)
 
-outbranch = "dep-multidocs"
-has_outbranch = true
+    outbranch = "dep-multidocs"
+    has_outbranch = true
 
-status_output = read(`git status --porcelain docs/Project.toml`, String)
-if !isempty(status_output)
-    @info "Restoring docs/Project.toml due to changes."
-    run(`git restore docs/Project.toml`)
-else
-    @info "No changes detected in docs/Project.toml."
-end
-
-if !success(`git checkout -f $outbranch`)
-    has_outbranch = false
-    if !success(`git switch --orphan $outbranch`)
-        @error "Cannot create new orphaned branch $outbranch."
-        exit(1)
-    end
-end
-
-@info "Cleaning up $gitroot."
-for file in readdir(gitroot; join = true)
-    file == "/home/runner/work/GraphNeuralNetworks.jl/GraphNeuralNetworks.jl/docs" && continue
-    endswith(file, ".git") && continue
-    rm(file; force = true, recursive = true)
-end
-
-@info "Copying aggregated documentation to $gitroot."
-for file in readdir(outpath)
-    cp(joinpath(outpath, file), joinpath(gitroot, file))
-end
-
-rm("/home/runner/work/GraphNeuralNetworks.jl/GraphNeuralNetworks.jl/docs"; force = true, recursive = true)
-
-run(`git add .`)
-if success(`git commit -m 'Aggregate documentation'`)
-    @info "Pushing updated documentation."
-    if has_outbranch
-        run(`git push`)
+    status_output = read(`git status --porcelain docs/Project.toml`, String)
+    if !isempty(status_output)
+        @info "Restoring docs/Project.toml due to changes."
+        run(`git restore docs/Project.toml`)
     else
-        run(`git push -u origin $outbranch`)
+        @info "No changes detected in docs/Project.toml."
     end
-    run(`git checkout master`)
-else
-    @info "No changes to aggregated documentation."
+
+    if !success(`git checkout -f $outbranch`)
+        has_outbranch = false
+        if !success(`git switch --orphan $outbranch`)
+            @error "Cannot create new orphaned branch $outbranch."
+            exit(1)
+        end
+    end
+
+    @info "Cleaning up $gitroot."
+    for file in readdir(gitroot; join = true)
+        file == "/home/runner/work/GraphNeuralNetworks.jl/GraphNeuralNetworks.jl/docs" && continue
+        endswith(file, ".git") && continue
+        rm(file; force = true, recursive = true)
+    end
+
+    @info "Copying aggregated documentation to $gitroot."
+    for file in readdir(outpath)
+        cp(joinpath(outpath, file), joinpath(gitroot, file))
+    end
+
+    rm("/home/runner/work/GraphNeuralNetworks.jl/GraphNeuralNetworks.jl/docs"; force = true, recursive = true)
+
+    run(`git add .`)
+    if success(`git commit -m 'Aggregate documentation'`)
+        @info "Pushing updated documentation."
+        if has_outbranch
+            run(`git push`)
+        else
+            run(`git push -u origin $outbranch`)
+        end
+        run(`git checkout master`)
+    else
+        @info "No changes to aggregated documentation."
+    end
 end
