f6d64716b26821c6893433e600663953c237ac38
Aurora Rossi <65721467+aurorarossi@users.noreply.github.com>
2024-11-07 23:14:29 +0100

    Add space
    Co-authored-by: Carlo Lucibello <carlo.lucibello@gmail.com>

--- GNNLux/src/layers/basic.jl
+++ GNNLux/src/layers/basic.jl
@@ -42,7 +42,7 @@ GNNGraph:
   num_nodes: 3
   num_edges: 6
 
-julia> ps, st = LuxCore.setup(rng,m);
+julia> ps, st = LuxCore.setup(rng, m);
 
 julia> m(g,x,ps,st)     # First entry is the output, second entry is the state of the model
 (Float32[-0.15594329 -0.15594329 -0.15594329; 0.93431795 0.93431795 0.93431795; 0.27568763 0.27568763 0.27568763; 0.12568939 0.12568939 0.12568939], (layer_1 = NamedTuple(), layer_2 = NamedTuple(), layer_3 = NamedTuple()))
