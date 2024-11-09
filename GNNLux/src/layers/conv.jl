6b0bf24d5fc4c5d05cf5d2630f807bb82e5bb30b
Aurora Rossi <aurora98rossi@gmail.com>
2024-11-09 15:06:22 +0100

    Add GINConv docs

--- GNNLux/src/layers/conv.jl
+++ GNNLux/src/layers/conv.jl
@@ -1272,6 +1272,51 @@ function Base.show(io::IO, l::GatedGraphConv)
     print(io, ")")
 end
 
+@doc raw"""
+    GINConv(f, ϵ; aggr=+)
+
+Graph Isomorphism convolutional layer from paper [How Powerful are Graph Neural Networks?](https://arxiv.org/pdf/1810.00826.pdf).
+
+Implements the graph convolution
+```math
+\mathbf{x}_i' = f_\Theta\left((1 + \epsilon) \mathbf{x}_i + \sum_{j \in N(i)} \mathbf{x}_j \right)
+```
+where ``f_\Theta`` typically denotes a learnable function, e.g. a linear layer or a multi-layer perceptron.
+
+# Arguments
+
+- `f`: A (possibly learnable) function acting on node features. 
+- `ϵ`: Weighting factor.
+
+# Examples:
+
+```julia
+using GNNLux, Lux, Random
+
+# initialize random number generator
+rng = Random.default_rng()
+
+# create data
+s = [1,1,2,3]
+t = [2,3,1,1]
+in_channel = 3
+out_channel = 5
+g = GNNGraph(s, t)
+x = randn(rng, Float32, in_channel, g.num_nodes)
+
+# create dense layer
+nn = Dense(in_channel, out_channel)
+
+# create layer
+l = GINConv(nn, 0.01f0, aggr = mean)
+
+# setup layer
+ps, st = LuxCore.setup(rng, l)
+
+# forward pass
+y, st = l(g, x, ps, st)       # size:  out_channel × num_nodes
+```
+"""
 @concrete struct GINConv <: GNNContainerLayer{(:nn,)}
     nn <: AbstractLuxLayer
     ϵ <: Real
