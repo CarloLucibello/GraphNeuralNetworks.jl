763a03d5663e0aaa084fc3a9a868572caa631e94
Aurora Rossi <aurora98rossi@gmail.com>
2024-11-09 20:19:50 +0100

    Add GConvGRU docs

--- GNNLux/src/layers/temporalconv.jl
+++ GNNLux/src/layers/temporalconv.jl
@@ -103,6 +103,49 @@ y, st = tgcn(g, x, ps, st)      # result size (6, 5)
 """
 TGCN(ch::Pair{Int, Int}; kwargs...) = GNNLux.StatefulRecurrentCell(TGCNCell(ch; kwargs...))
 
+"""
+    A3TGCN(in => out; use_bias = true, init_weight = glorot_uniform, init_state = zeros32, init_bias = zeros32, add_self_loops = false, use_edge_weight = true) 
+
+Attention Temporal Graph Convolutional Network (A3T-GCN) model from the paper [A3T-GCN: Attention Temporal Graph
+Convolutional Network for Traffic Forecasting](https://arxiv.org/pdf/2006.11583.pdf).
+
+Performs a TGCN layer, followed by a soft attention layer.
+
+# Arguments
+
+- `in`: Number of input features.
+- `out`: Number of output features.
+- `use_bias`: Add learnable bias. Default `true`.
+- `init_weight`: Weights' initializer. Default `glorot_uniform`.
+- `init_state`: Initial state of the hidden stat of the GRU layer. Default `zeros32`.
+- `init_bias`: Bias initializer. Default `zeros32`.
+- `add_self_loops`: Add self loops to the graph before performing the convolution. Default `false`.
+- `use_edge_weight`: If `true`, consider the edge weights in the input graph (if available).
+                     If `add_self_loops=true` the new weights will be set to 1. 
+                     This option is ignored if the `edge_weight` is explicitly provided in the forward pass.
+                     Default `false`.
+# Examples
+
+```julia
+using GNNLux, Lux, Random
+
+# initialize random number generator
+rng = Random.default_rng()
+
+# create data
+g = rand_graph(rng, 5, 10)
+x = rand(rng, Float32, 2, 5)
+
+# create A3TGCN layer
+l = A3TGCN(2 => 6)
+
+# setup layer
+ps, st = LuxCore.setup(rng, l)
+
+# forward pass
+y, st = l(g, x, ps, st)      # result size (6, 5)
+```
+"""
 @concrete struct A3TGCN <: GNNContainerLayer{(:tgcn, :dense1, :dense2)}
     in_dims::Int
     out_dims::Int
@@ -139,7 +182,7 @@ function Base.show(io::IO, l::A3TGCN)
     print(io, "A3TGCN($(l.in_dims) => $(l.out_dims))")
 end
 
-@concrete struct GConvGRUCell <: GNNContainerLayer{(:conv_x_r, :conv_h_r, :conv_x_z, :conv_h_z, :conv_x_h, :conv_h_h)}
+ @concrete struct GConvGRUCell <: GNNContainerLayer{(:conv_x_r, :conv_h_r, :conv_x_z, :conv_h_z, :conv_x_h, :conv_h_h)}
     in_dims::Int
     out_dims::Int
     k::Int
@@ -192,6 +235,45 @@ end
 
 LuxCore.outputsize(l::GConvGRUCell) = (l.out_dims,)
 
+"""
+    GConvGRU(in => out, k; use_bias = true, init_weight = glorot_uniform, init_state = zeros32, init_bias = zeros32) 
+
+Graph Convolutional Gated Recurrent Unit (GConvGRU) recurrent layer from the paper [Structured Sequence Modeling with Graph Convolutional Recurrent Networks](https://arxiv.org/pdf/1612.07659).
+
+Performs a layer of ChebConv to model spatial dependencies, followed by a Gated Recurrent Unit (GRU) cell to model temporal dependencies.
+
+# Arguments
+
+- `in`: Number of input features.
+- `out`: Number of output features.
+- `k`: Chebyshev polynomial order.
+- `use_bias`: Add learnable bias. Default `true`.
+- `init_weight`: Weights' initializer. Default `glorot_uniform`.
+- `init_state`: Initial state of the hidden stat of the GRU layer. Default `zeros32`.
+- `init_bias`: Bias initializer. Default `zeros32`.
+
+# Examples
+
+```julia
+using GNNLux, Lux, Random
+
+# initialize random number generator
+rng = Random.default_rng()
+
+# create data
+g = rand_graph(rng, 5, 10)
+x = rand(rng, Float32, 2, 5)
+
+# create A3TGCN layer
+l = GConvGRU(2 => 5, 2)
+
+# setup layer
+ps, st = LuxCore.setup(rng, l)
+
+# forward pass
+y, st = l(g, x, ps, st)      # result size (5, 5)
+```
+"""
 GConvGRU(ch::Pair{Int, Int}, k::Int; kwargs...) = GNNLux.StatefulRecurrentCell(GConvGRUCell(ch, k; kwargs...))
 
 @concrete struct GConvLSTMCell <: GNNContainerLayer{(:conv_x_i, :conv_h_i, :dense_i, :conv_x_f, :conv_h_f, :dense_f, :conv_x_c, :conv_h_c, :dense_c, :conv_x_o, :conv_h_o, :dense_o)}
