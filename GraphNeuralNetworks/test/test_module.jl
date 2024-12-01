b53289ba9a5b92eb1e532fab8f6cd5897b80f6bc
CarloLucibello <carlo.lucibello@gmail.com>
2024-12-01 15:07:17 +0100

    cleanup

--- GraphNeuralNetworks/test/test_module.jl
+++ GraphNeuralNetworks/test/test_module.jl
@@ -3,7 +3,7 @@
 using Pkg
 
 ## Uncomment below to change the default test settings
-ENV["GNN_TEST_CUDA"] = "true"
+# ENV["GNN_TEST_CUDA"] = "true"
 # ENV["GNN_TEST_AMDGPU"] = "true"
 # ENV["GNN_TEST_Metal"] = "true"
 
