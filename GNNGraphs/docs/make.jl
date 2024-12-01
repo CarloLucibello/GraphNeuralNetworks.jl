77bc14a4196d2ff298c0a29b5a607a6b5c792b54
CarloLucibello <carlo.lucibello@gmail.com>
2024-12-01 20:43:33 +0100

    gnngraphs

--- GNNGraphs/docs/make.jl
+++ GNNGraphs/docs/make.jl
@@ -4,29 +4,48 @@ using GNNGraphs
 import Graphs
 using Graphs: induced_subgraph
 
-assets=[]
-prettyurls = get(ENV, "CI", nothing) == "true"
-mathengine = MathJax3()
 
+DocMeta.setdocmeta!(GNNGraphs, :DocTestSetup, :(using GNNGraphs, MLUtils); recursive = true)
+
+mathengine = MathJax3(Dict(:loader => Dict("load" => ["[tex]/require", "[tex]/mathtools"]),
+    :tex => Dict("inlineMath" => [["\$", "\$"], ["\\(", "\\)"]],
+        "packages" => [
+            "base",
+            "ams",
+            "autoload",
+            "mathtools",
+            "require"
+        ])))
 
 makedocs(;
-         modules = [GNNGraphs],
-         doctest = false,
-         clean = true,
-         format = Documenter.HTML(; mathengine, prettyurls, assets = assets, size_threshold=nothing),
-         sitename = "GNNGraphs.jl",
-         pages = ["Home" => "index.md",
-            "Guides" => [
-               "Graphs" => ["guides/gnngraph.md", "guides/heterograph.md", "guides/temporalgraph.md"],
-               "Datasets" => "guides/datasets.md",
-            ],
-            "API Reference" => [
-                 "GNNGraph" => "api/gnngraph.md",
-                 "GNNHeteroGraph" => "api/heterograph.md",
-                 "TemporalSnapshotsGNNGraph" => "api/temporalgraph.md",
-                 "Samplers" => "api/samplers.md",
-              ],      
-         ]
-         )
+    modules = [GNNGraphs],
+    doctest = false, # TODO enable doctest
+    format = Documenter.HTML(; mathengine, 
+                    prettyurls = get(ENV, "CI", nothing) == "true", 
+                    assets = [],
+                    size_threshold=nothing, 
+                    size_threshold_warn=200000),sitename = "GNNGraphs.jl",
+    pages = [
+    "Home" => "index.md",
+    
+    "Guides" => [
+        "Graphs" => [
+        "guides/gnngraph.md", 
+        "guides/heterograph.md", 
+        "guides/temporalgraph.md"
+        ],
+        "Datasets" => "guides/datasets.md",
+    ],
+
+    "API Reference" => [
+        "GNNGraph" => "api/gnngraph.md",
+        "GNNHeteroGraph" => "api/heterograph.md",
+        "TemporalSnapshotsGNNGraph" => "api/temporalgraph.md",
+        "Samplers" => "api/samplers.md",
+        ],      
+    ]
+)
          
-deploydocs(;repo = "github.com/JuliaGraphs/GraphNeuralNetworks.jl.git", devbranch = "master", dirname = "GNNGraphs")
+deploydocs(repo = "github.com/JuliaGraphs/GraphNeuralNetworks.jl.git", 
+         devbranch = "master", 
+         dirname = "GNNGraphs")
