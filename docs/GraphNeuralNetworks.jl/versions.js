97b36fd89688dae3d5c8f203eafe2ae333934f5a
Documenter.jl <documenter@juliadocs.github.io>
2024-12-09 01:46:15 +0000

    build based on 2811969

--- docs/GraphNeuralNetworks.jl/versions.js
+++ docs/GraphNeuralNetworks.jl/versions.js
@@ -1,5 +1,12 @@
 var DOC_VERSIONS = [
+  "stable",
+  "v0.6",
+  "v0.5",
+  "v0.4",
+  "v0.3",
+  "v0.2",
+  "v0.1",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "dev";
-var DOCUMENTER_STABLE = "dev";
+var DOCUMENTER_NEWEST = "v0.6.21";
+var DOCUMENTER_STABLE = "stable";
