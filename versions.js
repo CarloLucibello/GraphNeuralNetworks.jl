e82c0e644d23780124434a3b330bccdf8b2ff2f9
Carlo Lucibello <carlo.lucibello@gmail.com>
2024-12-09 02:36:09 +0100

    cleanup

--- versions.js
+++ versions.js
@@ -1,5 +1,12 @@
 var DOC_VERSIONS = [
+  "stable",
   "dev",
+  "v0.1",
+  "v0.2",
+  "v0.3",
+  "v0.4",
+  "v0.5",
+  "v0.6",
 ];
-var DOCUMENTER_NEWEST = "dev";
-var DOCUMENTER_STABLE = "dev";
+var DOCUMENTER_NEWEST = "stable";
+var DOCUMENTER_STABLE = "stable";
